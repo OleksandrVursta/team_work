b40d425db990c16cf77980adfd4f393501438d0d
Arsen <arsen.v.medvid@ukd.edu.ua>
2026-05-01 15:18:55 +0300

    Add edit dish logic

--- menu.py
+++ menu.py
@@ -51,3 +51,24 @@ def main():
 
 
 main()
+
+def edit_dish():
+    name=input("Яку страву редагувати: ")
+
+    for dish in menu:
+        if dish["name"]==name:
+            dish["price"]=float(input("Нова ціна: "))
+            dish["description"]=input("Новий опис: ")
+            dish["category"]=input("Нова категорія: ")
+            print("Оновлено")
+            return
+
+    print("Страву не знайдено")
+
+def show_by_category():
+    category=input("Категорія: ")
+
+    for dish in menu:
+        if dish["category"]==category:
+            print(dish["name"])
+
